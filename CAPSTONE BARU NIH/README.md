866946ef443c2f2f63e148f06b3e94cefba5f2fb
FransiscaMayang <mayangfransisca1804@gmail.com>
2024-05-27 20:44:32 +0700

    mayang Edit

---
+++ CAPSTONE BARU NIH/README.md
@@ -0,0 +1,4 @@
+# km-feb24-section-semarang-team-19
+km-feb24-section-semarang-team-19 created by GitHub Classroom
+
+Ini adalah repository Section Semarang - Team 19
---
+++ CAPSTONE BARU NIH/script.js
@@ -0,0 +1,296 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const globalFilter = document.getElementById('global-filter');
+
+    globalFilter.addEventListener('change', function () {
+        const selectedValue = globalFilter.value;
+        updateCharts(selectedValue);
+    });
+
+    function updateCharts(selectedValue) {
+        // Update Line Chart
+        lineChart.data.datasets.forEach((dataset) => {
+            dataset.data = getDataForChart(selectedValue);
+        });
+        lineChart.update();
+
+        // Update Bar Chart Horizontal
+        barChartHorizontal.data.datasets.forEach((dataset) => {
+            dataset.data = getDataForChart(selectedValue);
+        });
+        barChartHorizontal.update();
+
+        // Update Bar Chart Vertical
+        barChartVertical.data.datasets.forEach((dataset) => {
+            dataset.data = getDataForChart(selectedValue);
+        });
+        barChartVertical.update();
+
+        // Update Pie Chart
+        myPieChart.data.datasets[0].data = [getDataForChart(selectedValue).reduce((a, b) => a + b, 0)];
+        myPieChart.update();
+
+        // Update Bar Chart Vertical 2
+        barChartVertical2.data.datasets.forEach((dataset) => {
+            dataset.data = getDataForChart(selectedValue);
+        });
+        barChartVertical2.update();
+
+        // Update Bar Chart Vertical 3
+        barChartVertical3.data.datasets.forEach((dataset) => {
+            dataset.data = getDataForChart(selectedValue);
+        });
+        barChartVertical3.update();
+    }
+
+    function getDataForChart(selectedValue) {
+        switch (selectedValue) {
+            case 'Astoria':
+                return [65, 28, 18];
+            case 'Hell\'s Kitchen':
+                return [59, 48, 48];
+            case 'Lower Manhattan':
+                return [80, 40, 77];
+            default:
+                return [65, 28, 18];
+        }
+    }
+
+    const lineCtx = document.getElementById('lineChart').getContext('2d');
+    const lineChart = new Chart(lineCtx, {
+        type: 'line',
+        data: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+            datasets: [
+                {
+                    label: 'Astoria',
+                    data: [65, 59, 80, 81, 56, 55],
+                    backgroundColor: 'rgba(255, 0, 0, 0.2)', // Merah
+                    borderColor: 'rgba(255, 0, 0, 1)', // Merah
+                    borderWidth: 1,
+                    fill: false
+                },
+                {
+                    label: 'Hell"s Kitchen',
+                    data: [28, 48, 40, 19, 86, 27],
+                    backgroundColor: 'rgba(0, 0, 255, 0.2)', // Biru
+                    borderColor: 'rgba(0, 0, 255, 1)', // Biru
+                    borderWidth: 1,
+                    fill: false
+                },
+                {
+                    label: 'Lower Manhattan',
+                    data: [18, 48, 77, 9, 100, 27],
+                    backgroundColor: 'rgba(0, 128, 0, 0.2)', // Hijau Tua
+                    borderColor: 'rgba(0, 128, 0, 1)', // Hijau Tua
+                    borderWidth: 1,
+                    fill: false
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                x: {
+                    beginAtZero: true
+                },
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+    // Horizontal Bar Chart for Produk Best Seller
+    const ctxBarHorizontal = document.getElementById('barChartHorizontal').getContext('2d');
+    const barChartHorizontal = new Chart(ctxBarHorizontal, {
+        type: 'bar',
+        data: {
+            labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+            datasets: [
+                {
+                    label: 'Asroria',
+                    data: [5, 15, 25, 35, 45, 55],
+                    backgroundColor: 'rgba(255, 0, 0, 0.2)', // Merah
+                    borderColor: 'rgba(255, 0, 0, 1)', // Merah
+                    borderWidth: 1
+                },
+                {
+                    label: 'Hell"s Kitchen',
+                    data: [10, 20, 30, 40, 50, 60],
+                    backgroundColor: 'rgba(0, 0, 255, 0.2)', // Biru
+                    borderColor: 'rgba(0, 0, 255, 1)', // Biru
+                    borderWidth: 1
+                },
+                {
+                    label: 'Lower Manhattan',
+                    data: [15, 25, 35, 45, 55, 65],
+                    backgroundColor: 'rgba(0, 128, 0, 0.2)', // Hijau Tua
+                    borderColor: 'rgba(0, 128, 0, 1)', // Hijau Tua
+                    borderWidth: 1
+                }
+            
+            ]
+        },
+        options: {
+            responsive: true,
+            indexAxis: 'y',
+            scales: {
+                x: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+    const ctxBarVertical = document.getElementById('barChartVertical').getContext('2d');
+    const barChartVertical = new Chart(ctxBarVertical, {
+        type: 'bar',
+        data: {
+            labels: ['Astoria', 'Hell"s Kitchen', 'Lower Manhattan'],
+            datasets: [
+                {
+                    label: '1-8 Dollar',
+                    data: [15, 25, 14, 25, 17],
+                    backgroundColor: 'rgba(255, 0, 0, 0.2)', // Merah
+                    borderColor: 'rgba(255, 0, 0, 1)', // Merah
+                    borderWidth: 1
+                },
+                {
+                    label: '9-16 Dollar',
+                    data: [2, 4, 6, 16, 20],
+                    backgroundColor: 'rgba(0, 0, 255, 0.2)', // Biru
+                    borderColor: 'rgba(0, 0, 255, 1)', // Biru
+                    borderWidth: 1
+                },
+                {
+                    label: '17-24 Dollar',
+                    data: [2, 4, 6, 7, 5],
+                    backgroundColor: 'rgba(0, 128, 0, 0.2)', // Hijau Tua
+                    borderColor: 'rgba(0, 128, 0, 1)', // Hijau Tua
+                    borderWidth: 1
+                },
+                {   label: 'Over 24 Dollar',
+                    data: [5, 8, 6, 2, 1],
+                    backgroundColor: 'rgba(150, 105, 25, 0.2)',
+                    borderColor: 'rgba(150, 105, 25, 1)',
+                    borderWidth: 1
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+    // Pie Chart for Total Revenue
+    const ctxPie = document.getElementById('myPieChart').getContext('2d');
+    const myPieChart = new Chart(ctxPie, {
+        type: 'pie',
+        data: {
+            labels: ['Astoria', 'Hell"s Kitchen', 'Lower Manhattan'],
+            datasets: [{
+                data: [300, 150, 100],
+                backgroundColor: [
+                    'rgba(255, 0, 0, 0.2)',
+                    'rgba(0, 0, 255, 0.2)',
+                    'rgba(0, 128, 0, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 0, 0, 1)',
+                    'rgba(0, 0, 255, 1)',
+                    'rgba(0, 128, 0, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+        }
+    });
+
+    // Vertical Bar Chart for Transaksi Per Bulan
+    const ctxBarVertical2 = document.getElementById('barChartVertical2').getContext('2d');
+    const barChartVertical2 = new Chart(ctxBarVertical2, {
+        type: 'bar',
+        data: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+            datasets: [
+                {
+                    label: 'Astoria',
+                    data: [10, 20, 30, 40, 50, 60],
+                    backgroundColor: 'rgba(255, 0, 0, 0.2)', // Merah
+                    borderColor: 'rgba(255, 0, 0, 1)', // Merah
+                    borderWidth: 1
+                },
+                {
+                    label: 'Hell"s Kitchen',
+                    data: [15, 25, 35, 45, 55, 65],
+                    backgroundColor: 'rgba(0, 0, 255, 0.2)', // Biru
+                    borderColor: 'rgba(0, 0, 255, 1)', // Biru
+                    borderWidth: 1
+                },
+                {
+                    label: 'Lower Manhattan',
+                    data: [20, 30, 40, 50, 60, 70],
+                    backgroundColor: 'rgba(0, 128, 0, 0.2)', // Hijau Tua
+                    borderColor: 'rgba(0, 128, 0, 1)', // Hijau Tua
+                    borderWidth: 1
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+    // Vertical Bar Chart for Transaksi Per Hari
+    const ctxBarVertical3 = document.getElementById('barChartVertical3').getContext('2d');
+    const barChartVertical3 = new Chart(ctxBarVertical3, {
+        type: 'bar',
+        data: {
+            labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+            datasets: [
+                {
+                    label: 'Astoria',
+                    data: [3, 5, 2, 8, 3, 7, 6],
+                    backgroundColor: 'rgba(255, 0, 0, 0.2)', // Merah
+                    borderColor: 'rgba(255, 0, 0, 1)', // Merah
+                    borderWidth: 1
+                },
+                {
+                    label: 'Hell"s Kitchen',
+                    data: [4, 6, 4, 6, 4, 6, 4],
+                    backgroundColor: 'rgba(0, 0, 255, 0.2)', // Biru
+                    borderColor: 'rgba(0, 0, 255, 1)', // Biru
+                    borderWidth: 1
+                },
+                {
+                    label: 'Lower Manhattan',
+                    data: [5, 7, 5, 7, 5, 7, 5],
+                    backgroundColor: 'rgba(0, 128, 0, 0.2)', // Hijau Tua
+                    borderColor: 'rgba(0, 128, 0, 1)', // Hijau Tua
+                    borderWidth: 1
+                }
+            ]
+        },
+        options: {
+        responsive: true,
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+    });
+});
