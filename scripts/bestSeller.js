d738a1f196920361cdd2ec8ebb591921422885d7
Defarhan Nugraha <defarhannugraha1@gmail.com>
2024-06-04 17:47:02 +0700

    edit fonts, dashboard, footer

--- scripts/bestSeller.js
+++ scripts/bestSeller.js
@@ -1,80 +1,123 @@
-fetch('./data/best_seller_looker.json')
-    .then(response => response.json())
-    .then(jsonData => {
-        // Sort data by Total_Quantity and take the top 10
-        const sortedData = jsonData.sort((a, b) => parseInt(b.Total_Quantity) - parseInt(a.Total_Quantity)).slice(0, 10);
+fetch("./data/best_seller_looker.json")
+  .then((response) => response.json())
+  .then((jsonData) => {
+    // Sort data by Total_Quantity
+    const sortedData = jsonData.sort(
+      (a, b) => parseInt(b.Total_Quantity) - parseInt(a.Total_Quantity)
+    );
 
-        // Extract unique product names from the sorted data
-        const productNames = sortedData.map(item => item.product_detail);
-        const uniqueProductNames = [...new Set(productNames)];
+    const tableBody = document.getElementById("best-seller-table-body");
 
-        // Separate data by store location
-        const astoriaProducts = sortedData.filter(item => item.store_location === 'Astoria');
-        const lowerManhattanProducts = sortedData.filter(item => item.store_location === 'Lower Manhattan');
-        const hellsKitchenProducts = sortedData.filter(item => item.store_location === 'Hells Kitchen');
+    sortedData.forEach((item) => {
+      const row = document.createElement("tr");
 
-        const astoriaData = uniqueProductNames.map(name => {
-            const product = astoriaProducts.find(item => item.product_detail === name);
-            return product ? parseInt(product.Total_Quantity) : 0;
-        });
+      row.innerHTML = `
+        <td>${item.product_detail}</td>
+        <td>${item.unit_price}</td>
+        <td>${item.store_location}</td>
+        <td>${item.Total_Quantity}</td>
+    `;
 
-        const lowerManhattanData = uniqueProductNames.map(name => {
-            const product = lowerManhattanProducts.find(item => item.product_detail === name);
-            return product ? parseInt(product.Total_Quantity) : 0;
-        });
+      tableBody.appendChild(row);
+    });
 
-        const hellsKitchenData = uniqueProductNames.map(name => {
-            const product = hellsKitchenProducts.find(item => item.product_detail === name);
-            return product ? parseInt(product.Total_Quantity) : 0;
-        });
+    $("#best-seller-table").DataTable({
+      columns: [
+        { data: "product_detail" },
+        {
+          data: "unit_price",
+          render: $.fn.dataTable.render.number(",", ".", 2, "$ "),
+        },
+        { data: "store_location" },
+        { data: "Total_Quantity" },
+      ],
+    });
 
-        const ctx = document.getElementById('stackedBarChartBestSeller').getContext('2d');
-        new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: uniqueProductNames,
-                datasets: [
-                    {
-                        label: 'Astoria',
-                        data: astoriaData,
-                        backgroundColor: 'rgba(75, 192, 192, 0.6)',
-                        borderColor: 'rgba(75, 192, 192, 1)',
-                        borderWidth: 1
-                    },
-                    {
-                        label: 'Lower Manhattan',
-                        data: lowerManhattanData,
-                        backgroundColor: 'rgba(153, 102, 255, 0.6)',
-                        borderColor: 'rgba(153, 102, 255, 1)',
-                        borderWidth: 1
-                    },
-                    {
-                        label: 'Hell\'s Kitchen',
-                        data: hellsKitchenData,
-                        backgroundColor: 'rgba(255, 99, 132, 0.6)',
-                        borderColor: 'rgba(255, 99, 132, 1)',
-                        borderWidth: 1
-                    }
-                ]
-            },
-            options: {
-                indexAxis: 'y', // This makes the chart horizontal
-                responsive: true,
-                plugins: {
-                    legend: {
-                        position: 'top'
-                    }
-                },
-                scales: {
-                    x: {
-                        stacked: true,
-                        beginAtZero: true
-                    },
-                    y: {
-                        stacked: true
-                    }
-                }
-            }
-        });
-    })
-    .catch(error => console.error('Error loading the JSON file:', error));
+    // Extract unique product names from the sorted data
+    const productNames = sortedData.map((item) => item.product_detail);
+    const uniqueProductNames = [...new Set(productNames)];
+
+    // Separate data by store location
+    const astoriaProducts = sortedData.filter(
+      (item) => item.store_location === "Astoria"
+    );
+    const lowerManhattanProducts = sortedData.filter(
+      (item) => item.store_location === "Lower Manhattan"
+    );
+    const hellsKitchenProducts = sortedData.filter(
+      (item) => item.store_location === "Hells Kitchen"
+    );
+
+    const astoriaData = uniqueProductNames.map((name) => {
+      const product = astoriaProducts.find(
+        (item) => item.product_detail === name
+      );
+      return product ? parseInt(product.Total_Quantity) : 0;
+    });
+
+    const lowerManhattanData = uniqueProductNames.map((name) => {
+      const product = lowerManhattanProducts.find(
+        (item) => item.product_detail === name
+      );
+      return product ? parseInt(product.Total_Quantity) : 0;
+    });
+
+    const hellsKitchenData = uniqueProductNames.map((name) => {
+      const product = hellsKitchenProducts.find(
+        (item) => item.product_detail === name
+      );
+      return product ? parseInt(product.Total_Quantity) : 0;
+    });
+
+    const ctx = document
+      .getElementById("stackedBarChartBestSeller")
+      .getContext("2d");
+    new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: uniqueProductNames,
+        datasets: [
+          {
+            label: "Astoria",
+            data: astoriaData,
+            backgroundColor: "rgba(75, 192, 192, 0.6)",
+            borderColor: "rgba(75, 192, 192, 1)",
+            borderWidth: 1,
+          },
+          {
+            label: "Lower Manhattan",
+            data: lowerManhattanData,
+            backgroundColor: "rgba(153, 102, 255, 0.6)",
+            borderColor: "rgba(153, 102, 255, 1)",
+            borderWidth: 1,
+          },
+          {
+            label: "Hell's Kitchen",
+            data: hellsKitchenData,
+            backgroundColor: "rgba(255, 99, 132, 0.6)",
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        indexAxis: "y", // This makes the chart horizontal
+        responsive: true,
+        plugins: {
+          legend: {
+            position: "top",
+          },
+        },
+        scales: {
+          x: {
+            stacked: true,
+            beginAtZero: true,
+          },
+          y: {
+            stacked: true,
+          },
+        },
+      },
+    });
+  })
+  .catch((error) => console.error("Error loading the JSON file:", error));
--- scripts/transaksiHarga.js
+++ scripts/transaksiHarga.js
@@ -1,61 +1,71 @@
-document.addEventListener('DOMContentLoaded', function () {
-    fetch('./data/transaksi_per_harga.json')
-        .then(response => response.json())
-        .then(data => {
-            const labels = ["Astoria", "Hells_Kitchen", "Lower_Manhattan"];
-            const priceRanges = ["1-8 Dollar", "9-16 Dollar", "17-24 Dollar", "Over 24 Dollar"];
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("./data/transaksi_per_harga.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const labels = ["Astoria", "Hells Kitchen", "Lower Manhattan"];
+      const priceRanges = [
+        "1-8 Dollar",
+        "9-16 Dollar",
+        "17-24 Dollar",
+        "Over 24 Dollar",
+      ];
 
-            const datasets = priceRanges.map((range, index) => ({
-                label: range,
-                data: labels.map(location => data.TransaksiHarga.find(item => item.Price_Range === range)[location]),
-                backgroundColor: getBackgroundColor(index),
-                stack: 'Stack 1'
-            }));
+      const datasets = priceRanges.map((range, index) => ({
+        label: range,
+        data: labels.map(
+          (location) =>
+            data.TransaksiHarga.find((item) => item.Price_Range === range)[
+              location
+            ]
+        ),
+        backgroundColor: getBackgroundColor(index),
+        stack: "Stack 1",
+      }));
 
-            const stackedBarConfig = {
-                type: 'bar',
-                data: {
-                    labels: labels,
-                    datasets: datasets
-                },
-                options: {
-                    responsive: true,
-                    plugins: {
-                        legend: {
-                            position: 'top',
-                        },
-                        tooltip: {
-                            mode: 'index',
-                            intersect: false,
-                        }
-                    },
-                    scales: {
-                        x: {
-                            stacked: true,
-                        },
-                        y: {
-                            stacked: true,
-                            beginAtZero: true,
-                            ticks: {
-                                stepSize: 1000 // Atur jika perlu
-                            }
-                        }
-                    }
-                }
-            };
+      const stackedBarConfig = {
+        type: "bar",
+        data: {
+          labels: labels,
+          datasets: datasets,
+        },
+        options: {
+          responsive: true,
+          plugins: {
+            legend: {
+              position: "top",
+            },
+            tooltip: {
+              mode: "index",
+              intersect: false,
+            },
+          },
+          scales: {
+            x: {
+              stacked: true,
+            },
+            y: {
+              stacked: true,
+              beginAtZero: true,
+              ticks: {
+                stepSize: 1000, // Atur jika perlu
+              },
+            },
+          },
+        },
+      };
 
-            const ctx = document.getElementById('stackedBarChart').getContext('2d');
-            new Chart(ctx, stackedBarConfig);
+      const ctx = document.getElementById("stackedBarChart").getContext("2d");
+      new Chart(ctx, stackedBarConfig);
 
-            function getBackgroundColor(index) {
-                const colors = [
-                    'rgba(54, 162, 235, 0.6)', // Biru
-                    'rgba(255, 99, 132, 0.6)', // Merah
-                    'rgba(75, 192, 192, 0.6)', // Hijau
-                    'rgba(153, 102, 255, 0.6)' // Ungu
-                ];
-                return colors[index % colors.length];
-            }
-        })
-        .catch(error => console.error('Error loading JSON data:', error));
+      function getBackgroundColor(index) {
+        const colors = [
+          "rgba(54, 162, 235, 0.6)", // Biru
+          "rgba(255, 99, 132, 0.6)", // Merah
+          "rgba(75, 192, 192, 0.6)", // Hijau
+          "rgba(153, 102, 255, 0.6)", // Ungu
+        ];
+        return colors[index % colors.length];
+      }
+    })
+    .catch((error) => console.error("Error loading JSON data:", error));
 });
